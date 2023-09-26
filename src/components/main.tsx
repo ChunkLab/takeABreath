75e22f47a51088e36ddd333df930aa4a58989f0b
axel <scampieamt@gmail.com>
2023-09-26 18:57:08 -0400

    Initialisation du projet

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -4,18 +4,11 @@ export function Main({ name = "Extension" }) {
   const [data, setData] = useState("")
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        padding: 16
-      }}>
+    <div>
       <h1>
-        Welcome to your <a href="https://www.plasmo.com">Plasmo</a> {name}!
+        Bonjour ! 
       </h1>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-
-      <a href="https://docs.plasmo.com">READ THE DOCS!</a>
+      
     </div>
   )
 }
